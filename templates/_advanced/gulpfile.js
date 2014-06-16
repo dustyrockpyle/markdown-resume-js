dada726196602567bf9030c0638ee038f3f47f13
dremonkey <a.deutmeyer@gmail.com>
2014-06-15 19:05:04 -0700

    added better error handling. added auto-reload when resume file changes

--- templates/_advanced/gulpfile.js
+++ templates/_advanced/gulpfile.js
@@ -1,5 +1,7 @@
 var args = require('yargs').argv;
+var clean = require('gulp-clean');
 var connect = require('gulp-connect');
+var fs = require('fs');
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
@@ -8,11 +10,18 @@ var rename = require("gulp-rename");
 var serverport = 8080;
 
 // Set the path to the resume you are building a theme for
-// var resume = "./test/resume.html";
 var filename = args.resume || 'resume.html';
 var resume = path.resolve(__dirname, '../../resumes/', filename);
 
-console.log(resume);
+// Make sure the resume file exists
+if (!fs.existsSync(resume)) {
+  throw new Error(resume + ' does not exists. Stopping...');
+}
+
+gulp.task('clean', function(cb){
+  return gulp.src('.tmp/index.html', {read: false})
+    .pipe(clean());
+});
 
 gulp.task('less', function() {
   return gulp.src('assets/less/resume.less')
@@ -25,12 +34,13 @@ gulp.task('less', function() {
 gulp.task('html', function() {
   return gulp.src(resume)
     .pipe(rename('index.html'))
-    .pipe(gulp.dest('.tmp/'))
+    .pipe(gulp.dest('.tmp'))
     .pipe(connect.reload());
 });
 
-gulp.task('watch', ['less'], function() {
+gulp.task('watch', ['html','less'], function() {
   gulp.watch('assets/less/{*,/}*.less', ['less']);
+  gulp.watch(resume, ['html']);
 });
 
 gulp.task('connect', function() {
@@ -41,5 +51,5 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('serve', ['html', 'watch', 'connect']);
+gulp.task('serve', ['watch', 'connect']);
 
